fbd185c48f13217ec4eb16cca096aef8d28219fe
Felipe Caldeira <hms1730105@gmail.com>
2022-03-22 14:14:59 -0500

    Changed build dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { NextPage } from 'next'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
 import MainLayout from '@layouts/Main'
 
 const messages: string[] = [
@@ -16,6 +16,8 @@ const Home: NextPage = () => {
             <Box borderRadius="lg" bg="red" p={3} w="40" textAlign="center">
                 Well hello there fancy people!!!
             </Box>
+
+            <Heading as='h2' fontWeight='normal'>About me</Heading>
 
         </MainLayout>
     )
